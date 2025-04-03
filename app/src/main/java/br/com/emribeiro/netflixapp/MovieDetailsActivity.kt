cc7b9368a1324b0ac198a30b34adad46447a17e9
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2025-04-03 10:41:08 -0300

    feat: [Movie Details Screen] - Create MovieDetailsActivity ; Create MovieCover With gradient background

---
+++ app/src/main/java/br/com/emribeiro/netflixapp/MovieDetailsActivity.kt
@@ -0,0 +1,35 @@
+package br.com.emribeiro.netflixapp
+
+import android.graphics.drawable.LayerDrawable
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_movie_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val toolbar: Toolbar = findViewById(R.id.app_toolbar)
+        setSupportActionBar(toolbar)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable : LayerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        layerDrawable.setDrawableByLayerId(R.id.image_cover, movieCover)
+        val coverImg: ImageView = findViewById(R.id.movie_cover)
+        coverImg.setImageDrawable(layerDrawable)
+    }
+}
