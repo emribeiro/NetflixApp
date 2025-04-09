34818b073fee05b66c2efb354b75dbf2ca5f3202
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2025-04-09 09:47:02 -0300

    feat: [Movie Details Screen] - Implement logic to fields and recyclerView

--- app/src/main/java/br/com/emribeiro/netflixapp/MovieDetailsActivity.kt
+++ app/src/main/java/br/com/emribeiro/netflixapp/MovieDetailsActivity.kt
@@ -3,12 +3,18 @@ package br.com.emribeiro.netflixapp
 import android.graphics.drawable.LayerDrawable
 import android.os.Bundle
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import br.com.emribeiro.netflixapp.model.Category
+import br.com.emribeiro.netflixapp.model.Movie
 
 class MovieDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -31,5 +37,22 @@ class MovieDetailsActivity : AppCompatActivity() {
         layerDrawable.setDrawableByLayerId(R.id.image_cover, movieCover)
         val coverImg: ImageView = findViewById(R.id.movie_cover)
         coverImg.setImageDrawable(layerDrawable)
+
+        val movieTitle: TextView = findViewById(R.id.movie_title)
+        val movieDescription: TextView = findViewById(R.id.movie_description)
+        val movieCast: TextView = findViewById(R.id.movie_cast)
+
+        movieTitle.text = "Batman: O Cavaleiro das Trevas Ressurge"
+        movieDescription.text = "Passaram-se oito anos desde a morte de Harvey Dent e o Batman, ao assumir a culpa pela morte do promotor, desapareceu. Agora, ele terá de lidar com a chegada da Mulher Gato e de Bane em Gotham City."
+        movieCast.text = "Christian Bale, Tom Hardy, Anne Hathaway e outros"
+        val movieList = mutableListOf<Movie>()
+        for(k in 1..12){
+            movieList.add(Movie("Movie $k", R.drawable.placeholder_bg))
+        }
+
+        val similarOptionsRv: RecyclerView = findViewById(R.id.other_options_rv)
+        similarOptionsRv.layoutManager = GridLayoutManager(this, 4)
+        similarOptionsRv.adapter = MovieRecyclerViewAdapter(movieList)
+
     }
 }
