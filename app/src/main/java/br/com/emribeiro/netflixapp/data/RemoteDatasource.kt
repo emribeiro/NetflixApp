f599ead8fe784e3dc94a45620baf880fc098eb5f
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2025-04-10 10:54:32 -0300

    feat: [Remote dataSource] - Convert JsonData in categories list

--- app/src/main/java/br/com/emribeiro/netflixapp/data/RemoteDatasource.kt
+++ app/src/main/java/br/com/emribeiro/netflixapp/data/RemoteDatasource.kt
@@ -1,6 +1,9 @@
 package br.com.emribeiro.netflixapp.data
 
 import android.util.Log
+import br.com.emribeiro.netflixapp.model.Category
+import br.com.emribeiro.netflixapp.model.Movie
+import org.json.JSONObject
 import java.io.IOException
 import java.net.URL
 import java.util.concurrent.Executors
@@ -27,7 +30,31 @@ class RemoteDatasource {
                 it.readText()
             }
 
-            Log.i("Dados", jsonData)
+            Log.i("Dados", toCategories(jsonData).toString())
         }
+    }
+
+    private fun toCategories(jsonString: String): List<Category>{
+        val categories = mutableListOf<Category>()
+        val jsonCategories = JSONObject(jsonString).getJSONArray("category")
+
+        for(i in 0 until jsonCategories.length()){
+            val jsonCategory = jsonCategories.getJSONObject(i)
+            val id = jsonCategory.getInt("id")
+            val title = jsonCategory.getString("title")
+            val jsonMovies = jsonCategory.getJSONArray("movie")
+            val movies = mutableListOf<Movie>()
+            for(j in 0 until jsonMovies.length()){
+                val jsonMovie = jsonMovies.getJSONObject(j)
+                val movieId = jsonMovie.getInt("id")
+                val coverUrl = jsonMovie.getString("cover_url")
+                movies.add(Movie(movieId, coverUrl))
+            }
+
+            categories.add(Category(id, title, movies))
+
+        }
+
+        return categories
     }
 }
