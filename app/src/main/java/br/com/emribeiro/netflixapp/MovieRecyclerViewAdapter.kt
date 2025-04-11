893aa843067f91875ae20cb064e922e3c5608bba
Eric Miranda Ribeiro <ktoxlonian@gmail.com>
2025-04-11 09:55:21 -0300

    feat: [MainActivity] - Include glide for image load and some layout fixes

--- app/src/main/java/br/com/emribeiro/netflixapp/MovieRecyclerViewAdapter.kt
+++ app/src/main/java/br/com/emribeiro/netflixapp/MovieRecyclerViewAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import br.com.emribeiro.netflixapp.model.Movie
+import com.bumptech.glide.Glide
 
 class MovieRecyclerViewAdapter(private val movies: List<Movie>): RecyclerView.Adapter<MovieRecyclerViewAdapter.MovieViewHolder>(){
 
@@ -27,7 +28,11 @@ class MovieRecyclerViewAdapter(private val movies: List<Movie>): RecyclerView.Ad
         fun bind(movie: Movie){
             val image: ImageView = itemView.findViewById(R.id.movie_image)
             image.contentDescription = movie.coverUrl
-            image.setImageResource(R.drawable.placeholder_bg)
+//            image.setImageResource(R.drawable.placeholder_bg)
+            Glide.with(itemView)
+                .load(movie.coverUrl)
+                .placeholder(R.drawable.placeholder_bg)
+                .into(image)
         }
     }
 }
